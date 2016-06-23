3e29ebfd27905ca88186fab5ff7d45eee0b7102f
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-06-23 19:20:19 +0400

    Added Printer

---
+++ Delegates/Events/Program.cs
@@ -0,0 +1,61 @@
+﻿using EventsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hp = new Printer();
+
+            hp.PrintStarted += Hp_PrintStarted;
+            hp.PrintFinished += Hp_PrintFinished;
+            hp.Printing += Hp_Printing;
+            hp.PrintFailed += Hp_PrintFailed;
+
+            try
+            {
+                hp.AddPapers(4);
+                hp.Print(2);
+                hp.Print(3);
+            }
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine($"Opps! Wront argument {exception.ParamName}.{Environment.NewLine}Message: {exception.Message}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static void Hp_PrintFailed(object sender, PrintFailedEventArgs e)
+        {
+            Console.WriteLine($"Print failed. Remaining {e.RemainingPages} pages");
+        }
+
+        private static void Hp_Printing(object sender, PrintingEventArgs e)
+        {
+            Console.WriteLine($"Printing page N{e.CurrentPage}");
+        }
+
+        private static void Hp_PrintFinished(object sender, EventArgs e)
+        {
+            Console.WriteLine("Print Finished");
+        }
+
+        private static void Hp_PrintStarted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Print Started");
+        }
+    }
+}
---
+++ Delegates/EventsLib/Printer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsLib
+{
+    public class PrintingEventArgs : EventArgs
+    {
+        public int CurrentPage { get; set; }
+    }
+
+    public class PrintFailedEventArgs : EventArgs
+    {
+        public int RemainingPages { get; set; }
+    }
+
+    public class Printer
+    {
+        public event EventHandler PrintStarted;
+        public event EventHandler PrintFinished;
+        public event EventHandler<PrintingEventArgs> Printing;
+        public event EventHandler<PrintFailedEventArgs> PrintFailed;
+
+        private void OnPrintStarted()
+        {
+            Thread.Sleep(1000);
+            PrintStarted?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnPrintFinished()
+        {
+            Thread.Sleep(1000);
+            PrintFinished?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnPrinting(int current)
+        {
+            var args = new PrintingEventArgs { CurrentPage = current };
+            Printing?.Invoke(this, args);
+            Thread.Sleep(1500);
+        }
+
+        private void OnPrintFailed(int remaining)
+        {
+            Thread.Sleep(3000);
+            var args = new PrintFailedEventArgs { RemainingPages = remaining };
+            PrintFailed?.Invoke(this, args);
+        }
+
+        public int PapersQuantity { get; private set; }
+
+        public void AddPapers(int papersCount)
+        {
+            if (papersCount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(papersCount),
+                    $"The argument {nameof(papersCount)} should be not negative.");
+            }
+
+            PapersQuantity += papersCount;
+        }
+
+        public void Print(int pagesCount)
+        {
+            if (pagesCount < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pagesCount),
+                    $"The argument {nameof(pagesCount)} should be greather than 0.");
+            }
+
+            OnPrintStarted();
+
+            for (int i = 1; i <= pagesCount; i++)
+            {
+                if (PapersQuantity != 0)
+                {
+                    OnPrinting(i);
+                    PapersQuantity--;
+                }
+                else
+                {
+                    OnPrintFailed(pagesCount - i + 1);
+                    break;
+                }
+            }
+
+            OnPrintFinished();
+        }
+    }
+}
