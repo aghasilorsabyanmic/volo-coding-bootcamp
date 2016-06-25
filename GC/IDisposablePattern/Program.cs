14d4b712316910a277fb2499276ce80501a4a34e
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-06-25 11:24:50 +0400

    Added GC Solution.

---
+++ GC/IDisposablePattern/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposablePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var obj = new MyClass())
+            using (var obj1 = new MyClass())
+            {
+                try
+                {
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            var obj2 = new MyClass();
+        }
+    }
+
+    class MyClass : IDisposable
+    {
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        //TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        ~MyClass()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
